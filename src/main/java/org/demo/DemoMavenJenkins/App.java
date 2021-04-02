429f26f971b172cdd2556b9e6c28c73d0bd26c6d
gkumar1987 <gyl_gaurav@yahoo.com>
2021-04-02 15:39:23 +0530

    first maven commit

---
+++ src/main/java/org/demo/DemoMavenJenkins/App.java
@@ -0,0 +1,13 @@
+package org.demo.DemoMavenJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
